e23bc785e5958ab0ef3ada2904550e5f222d960b
HeartBeats <913305160@qq.com>
2022-04-26 11:38:17 +0800

    添加沉浸式状态栏的设置和取消方法

--- libs/project/base/arch/src/main/java/com/hl/arch/base/BaseActivity.kt
+++ libs/project/base/arch/src/main/java/com/hl/arch/base/BaseActivity.kt
@@ -42,7 +42,7 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate: ")
+        Log.d(TAG, "onCreate:  =====> $this")
 
         layoutResId?.let {
             val inflateView = View.inflate(this, it, null)
@@ -64,6 +64,35 @@ abstract class BaseActivity : AppCompatActivity() {
         onViewCreated(savedInstanceState)
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: =====> $this ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart:  =====> $this")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume:  =====> $this")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause:  =====> $this")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop:  =====> $this")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy:  =====> $this")
+    }
 
     /**
      * 旋屏之后，随着 onPause 的执行，onSaveInstanceState 会被执行， Activity 会自动保存当前页面的状态数据
