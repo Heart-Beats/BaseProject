e12103a00640d6ccec36ae25a483a12be423d488
张磊 <zhanglei@youmatech.com>
2022-07-14 23:04:56 +0800

    IStandSdkImpl 添加 setWebView、previewImage 以及 savePhotoToAlbum 方法的实现， 优化 navigateBack 方法实现逻辑

---
+++ libs/project/base/arch/src/main/java/com/hl/arch/web/helpers/_CallBackFunction.kt
@@ -0,0 +1,25 @@
+package com.hl.arch.web.helpers
+
+import com.github.lzyzsd.jsbridge.CallBackFunction
+import com.hl.arch.web.bean.H5Return
+
+/**
+ * @author  张磊  on  2022/07/14 at 22:07
+ * Email: 913305160@qq.com
+ */
+
+fun <T> CallBackFunction.onSuccess(data: T) {
+	this.onCallBack(H5Return.success(data))
+}
+
+fun <T> CallBackFunction.onFail(data: T) {
+	this.onCallBack(H5Return.fail(data))
+}
+
+fun CallBackFunction.onSuccess() {
+	this.onSuccess("")
+}
+
+fun CallBackFunction.onFail() {
+	this.onFail("")
+}
