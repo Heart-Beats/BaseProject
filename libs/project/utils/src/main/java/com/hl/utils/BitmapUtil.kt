5ff7e6953b082e6e7218d7f038d4b067fb025e54
张磊 <zhanglei@youmatech.com>
2022-08-12 14:58:02 +0800

    保存图片到图库实现更改，ScanFileActionUtil  添加复制图片到公共目录再扫描的方法

--- libs/project/utils/src/main/java/com/hl/utils/BitmapUtil.kt
+++ libs/project/utils/src/main/java/com/hl/utils/BitmapUtil.kt
@@ -1,12 +1,17 @@
 package com.hl.utils
 
+import android.Manifest
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Environment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.lifecycleScope
 import com.blankj.utilcode.util.ImageUtils
 import com.elvishew.xlog.XLog
+import com.hl.uikit.reqPermissions
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.InputStream
@@ -42,35 +47,96 @@ object BitmapUtil {
         }
     }
 
-    suspend fun saveBitmap(
+    /**
+     * 保存 bitmap 图片文件
+     *
+     * @param bitmap            bitmap
+     * @param saveName          保存图片名称
+     * @param isSave2AppDir     是否保存在 App 私有目录下
+     *@param  failAction        失败回调
+     *@param  successAction     成功回调
+     *
+     */
+    fun saveBitmap(
         context: Context,
         bitmap: Bitmap,
         saveName: String,
+        isSave2AppDir: Boolean = false,
         failAction: (errorMsg: String) -> Unit = {},
         successAction: () -> Unit = {}
     ) {
-        val saveFile = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.absolutePath, saveName)
-        val isSave = withContext(Dispatchers.IO) {
-            ImageUtils.save(bitmap, saveFile, Bitmap.CompressFormat.PNG)
+
+        val saveFile = if (isSave2AppDir) {
+            File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), saveName)
+        } else {
+            File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), saveName)
         }
 
-        if (isSave) {
-            ScanFileActionUtil.scanMedia(context, saveFile.absolutePath,
-                scanResultCallBack = object : ScanResultCallBack {
-                    override fun onScanSuccess(scanFile: File) {
-                        successAction()
+        saveBitmap(context, bitmap, saveFile, isSave2AppDir, failAction, successAction)
+    }
+
+    /**
+     * 保存 bitmap 图片文件
+     *
+     * @param bitmap            bitmap
+     * @param saveFile          保存的图片文件
+     * @param isSave2AppDir     是否保存在 App 私有目录下， false: saveFile 若在私有目录下会被拷贝到公共目录
+     *@param  failAction        失败回调
+     *@param  successAction     成功回调
+     *
+     */
+    fun saveBitmap(
+        context: Context,
+        bitmap: Bitmap,
+        saveFile: File,
+        isSave2AppDir: Boolean = false,
+        failAction: (errorMsg: String) -> Unit = {},
+        successAction: () -> Unit = {}
+    ) {
+        if (context is FragmentActivity) {
+            context.reqPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, deniedAction = {
+                failAction("拒绝权限，插入到图库失败")
+            }) {
+                context.lifecycleScope.launch {
+                    val isSave = withContext(Dispatchers.IO) {
+                        ImageUtils.save(bitmap, saveFile, Bitmap.CompressFormat.PNG)
                     }
 
-                    override fun onScanFail(errorMsg: String) {
-                        failAction(errorMsg)
-                    }
+                    if (isSave) {
+                        val scanResultCallBack = object : ScanResultCallBack {
+                            override fun onScanSuccess(scanFile: File) {
+                                successAction()
+                            }
 
-                    override fun onScanInfo(msg: String) {
-                        XLog.d("onScanInfo: $msg")
+                            override fun onScanFail(errorMsg: String) {
+                                failAction(errorMsg)
+                            }
+
+                            override fun onScanInfo(msg: String) {
+                                XLog.d("onScanInfo: $msg")
+                            }
+                        }
+
+                        if (isSave2AppDir) {
+                            ScanFileActionUtil.scanMedia(
+                                context,
+                                saveFile.absolutePath,
+                                scanResultCallBack = scanResultCallBack
+                            )
+                        } else {
+                            ScanFileActionUtil.scanMedia2Public(
+                                context,
+                                saveFile.absolutePath,
+                                scanResultCallBack = scanResultCallBack
+                            )
+                        }
+                    } else {
+                        failAction("保存图片失败")
                     }
-                })
+                }
+            }
         } else {
-            failAction("保存图片失败")
+            failAction("context 非 FragmentActivity 类型")
         }
     }
 }
