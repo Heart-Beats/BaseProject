154c573a3fc73366ae09c3c6f7f90dc69a5beecd
张磊 <zhanglei@youmatech.com>
2022-09-02 20:44:13 +0800

    ActivityResultHelper 优化对启动 Intent 的处理

--- libs/project/utils/src/main/java/com/hl/utils/activityResult/MyStartActivityForResult.kt
+++ libs/project/utils/src/main/java/com/hl/utils/activityResult/MyStartActivityForResult.kt
@@ -1,6 +1,5 @@
 package com.hl.utils.activityResult
 
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import androidx.activity.result.ActivityResult
@@ -12,12 +11,11 @@ import androidx.activity.result.contract.ActivityResultContract
  */
 class MyStartActivityForResult : ActivityResultContract<Intent, ActivityResult>() {
 
-	private var inputComponent: ComponentName? = null
+	// intent 的标识符
+	private var intentIdentifier: String? = null
 
 	override fun createIntent(context: Context, input: Intent): Intent {
-
-		inputComponent = input.component
-
+		intentIdentifier = input.identifier
 		return input
 	}
 
@@ -28,9 +26,10 @@ class MyStartActivityForResult : ActivityResultContract<Intent, ActivityResult>(
 
 		// 这里保证 intent 不为空，因为在取消返回的情况下 intent 会为 null, 此时就会无法取得 intent 对应的 component
 		val data = intent ?: Intent()
+
 		val activityResult = ActivityResult(resultCode, data.apply {
-			// 设置返回数据时的 component， 该 component 为启动时对应的Activity
-			this.component = inputComponent
+			// 设置返回数据 intent 的 identifier 唯一标识符， 该 identifier 等于启动的 Intent 的 identifier
+			this.identifier = intentIdentifier
 		})
 		return activityResult
 	}
