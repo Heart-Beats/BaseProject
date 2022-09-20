c8d700bc0f61c7aaa81e7f3a21d2ee4de0fbc445
张磊 <zhanglei@youmatech.com>
2022-09-20 21:02:29 +0800

    添加 Shadow 多进程加载插件的 PluginManager 的实现

--- libs/project/SDK/Shadow/shadow-init/src/main/java/com/hl/shadow/Shadow.kt
+++ libs/project/SDK/Shadow/shadow-init/src/main/java/com/hl/shadow/Shadow.kt
@@ -5,12 +5,13 @@ import com.hl.shadow.logger.AndroidLoggerFactory
 import com.hl.shadow.logger.LogLevel
 import com.hl.shadow.logger.OnShadowLog
 import com.hl.shadow.managerupdater.MyPluginManagerUpdater
+import com.hl.shadow.pluginmanager.MyPluginManager
+import com.hl.shadow.pluginmanager.ProcessPluginManager
 import com.tencent.shadow.core.common.LoggerFactory
 import com.tencent.shadow.dynamic.host.DynamicPluginManager
 import com.tencent.shadow.dynamic.host.PluginManager
 import com.tencent.shadow.dynamic.host.PluginManagerImpl
-import com.hl.shadow.pluginmanager.MyPluginManager
-import com.tencent.shadow.dynamic.manager.MyDynamicUuidPluginManager
+import com.tencent.shadow.dynamic.host.PluginProcessService
 import java.io.File
 import java.util.concurrent.Future
 
@@ -32,6 +33,13 @@ object Shadow {
 	 * 静态实现的插件管理类
 	 */
 	private var pluginManagerImpl: PluginManagerImpl? = null
+
+
+	/**
+	 * 多进程 PluginManager 的映射
+	 */
+	private val processPluginManagerMap = mutableMapOf<String, ProcessPluginManager>()
+
 
 	/**
 	 * 初始化 Shadow 的 log 打印
@@ -56,14 +64,36 @@ object Shadow {
 			}
 		} ?: throw ExceptionInInitializerError("请先调用 initShadowLog 方法初始化 LoggerFactory")
 
-		if (pluginManagerImpl == null) {
-			// 动态获取是通过反射机制获取的
+		if (pluginManagerImpl == null) {            // 动态获取是通过反射机制获取的
 			// val className = "com.tencent.shadow.dynamic.impl.ManagerFactoryImpl"
 			// val newInstance = Class.forName(className).newInstance()
 			// pluginManagerImpl = ManagerFactory::class.cast(newInstance).buildManager(context)
 			pluginManagerImpl = MyPluginManager(context)
 		}
 		return pluginManagerImpl
+	}
+
+	/**
+	 * 获取静态的 PluginManager， 无需动态 APK 进行加载
+	 *
+	 * @param  context           上下文对象
+	 * @return PluginManager
+	 */
+	fun getProcessPluginManager(context: Context, managerName: String, ppsName: String): PluginManager? {
+		androidLoggerFactory?.run {
+			try {
+				LoggerFactory.getILoggerFactory()
+			} catch (e: Exception) {
+				LoggerFactory.setILoggerFactory(this)
+			}
+		} ?: throw ExceptionInInitializerError("请先调用 initShadowLog 方法初始化 LoggerFactory")
+
+		if (processPluginManagerMap[managerName] == null) {
+			val processPluginManager = ProcessPluginManager(context, managerName, ppsName)
+			processPluginManagerMap[managerName] = processPluginManager
+		}
+
+		return processPluginManagerMap[managerName]
 	}
 
 
