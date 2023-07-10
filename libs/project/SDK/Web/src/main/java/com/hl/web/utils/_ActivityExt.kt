51240df76c9a70bba9c758244305ea75e60d7c0f
张磊 <913305160@qq.com>
2023-07-10 09:25:24 +0800

    修复 getCurrentNavigationFragment 方法获取 navigation 当前使用的 Fragment 错误的问题

--- libs/project/SDK/Web/src/main/java/com/hl/web/utils/_ActivityExt.kt
+++ libs/project/SDK/Web/src/main/java/com/hl/web/utils/_ActivityExt.kt
@@ -8,5 +8,5 @@ import androidx.fragment.app.FragmentActivity
  * 获取 navigation 当前使用的 Fragment
  */
 fun FragmentActivity.getCurrentNavigationFragment(): Fragment? {
-	return supportFragmentManager.primaryNavigationFragment?.childFragmentManager?.fragments?.firstOrNull()
+	return supportFragmentManager.primaryNavigationFragment?.childFragmentManager?.fragments?.lastOrNull()
 }
