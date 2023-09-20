173eb543ef8eb6439b6991cae3d21eef09a590a0
张磊 <913305160@qq.com>
2023-09-20 17:29:22 +0800

    正则校验添加 IPV4 与 IPV6, 设置 TextView 最大长度优化以及扩展支持添加 inputFilter， 发布测试版本 0.0.2

--- libs/uikit/uikit/src/main/java/com/hl/uikit/utils/_TextView.kt
+++ libs/uikit/uikit/src/main/java/com/hl/uikit/utils/_TextView.kt
@@ -1,12 +1,15 @@
 package com.hl.uikit.utils
 
 import android.text.InputFilter
-import android.text.TextUtils
 import android.widget.TextView
 
 /**
  * @author  张磊  on  2023/09/07 at 17:38
  * Email: 913305160@qq.com
+ */
+
+/**
+ * 设置 TextView 最大长度
  */
 fun TextView.setMaxLength(length: Int) {
 	val filters = this.filters.toMutableList()
@@ -14,5 +17,14 @@ fun TextView.setMaxLength(length: Int) {
 	filters.removeAll { it is InputFilter.LengthFilter }
 	filters.add(InputFilter.LengthFilter(length))
 	this.filters = filters.toTypedArray()
-	this.ellipsize = TextUtils.TruncateAt.END
+}
+
+/**
+ * 给 TextView 添加 inputFilter
+ */
+fun TextView.addInputFilter(inputFilter: InputFilter) {
+	val filters = this.filters.toMutableList().apply {
+		add(inputFilter)
+	}
+	this.filters = filters.toTypedArray()
 }
